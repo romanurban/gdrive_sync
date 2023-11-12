213f9d5a9e63f5f7235716223d6a8c81d29ec675
Roman Urban-Orban <roman.urban.orban@gmail.com>
2023-11-12 16:03:15 +0200

    some fixes

--- sync.sh
+++ sync.sh
@@ -11,17 +11,16 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Get the directory of the script
+# get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Source the configuration file from the same directory as the script
 source "$SCRIPT_DIR/sync_settings.conf"
 
-# Use variables from config file
+# use variables from config file
 local_folder="$LOCAL_FOLDER"
 drive_base_folder="$DRIVE_BASE_FOLDER"
 drive_folder_name="$DRIVE_FOLDER_NAME"
-ignore_list=($IGNORE_LIST) # Convert space-separated list to array
+ignore_list=($IGNORE_LIST) # convert space-separated list to array
 drive_folder="$drive_base_folder/$drive_folder_name"
 
 log() {
@@ -30,16 +29,15 @@ log() {
         echo "$message"
     fi
 }
-# Function to initialize progress
+
 init_progress() {
+	echo "Starting synchronization..."
+	echo "Counting items to synchronize."
     total_items=$(count_items "$drive_folder")
     current_item=0
-	toggle=0
-    echo "Starting synchronization..."
     echo "Total items to synchronize: $total_items"
 }
 
-# Function to count items recursively
 count_items() {
     local dir="$1"
     local count=0
@@ -62,43 +60,31 @@ count_items() {
     echo $count
 }
 
-# Function to update progress
+# update on a progress in console
 update_progress() {
     if [ "$verbose" -eq 0 ]; then
-        # Increment the item counter
         ((current_item++))
 
-        # Toggle the symbol
-        if [ "$toggle" -eq 0 ]; then
-            symbol="/"
-            toggle=1
-        else
-            symbol="\\"
-            toggle=0
-        fi
+		case $((current_item % 3)) in
+			0) symbol="/" ;;
+			1) symbol="|" ;;
+			2) symbol="\\" ;;
+		esac
+		printf "\rProcessed $current_item/$total_items items $symbol"
 
-        # Display progress with the symbol
-        printf "\rProcessed $current_item/$total_items items $symbol"
-
-        # Print a new line every 50 items or at the end
-        if ((current_item % 50 == 0)) || [ "$current_item" -eq "$total_items" ]; then
-            echo ""
-        fi
     fi
 }
 
-# Function to check if a directory is in the ignore list
 is_ignored() {
     local item=$1
     for ignored_item in "${ignore_list[@]}"; do
         if [[ "$item" == "$ignored_item" ]]; then
-            return 0 # True, item is ignored
+            return 0 # item is ignored
         fi
     done
-    return 1 # False, item is not ignored
+    return 1 # item is not ignored
 }
 
-# Function to copy a file
 copy_file() {
     local source="$1"
     local target="$2"
@@ -108,20 +94,18 @@ copy_file() {
     gio copy "$source" "$target"
 }
 
-# Function to get the display name using gio info
 get_display_name() {
     local path="$1"
     gio info "$path" | grep "display name:" | cut -d' ' -f3-
 }
 
-# Function to recursively process items
 process_directory() {
     local source_dir="$1"
     local target_dir="$2"
 
     log "Processing directory: $source_dir"
 
-    # Create the target directory
+    # create the target dir if not exists
     mkdir -p "$target_dir"
 
     local item
@@ -148,18 +132,41 @@ process_directory() {
     done
 }
 
-# Start the sync process
+display_elapsed_time() {
+    local start_time=$1
+    local end_time=$(date +%s)
+    local elapsed=$((end_time - start_time))
+
+    local hours=$((elapsed / 3600))
+    local minutes=$(( (elapsed % 3600) / 60 ))
+    local seconds=$((elapsed % 60))
+
+    local time_string=""
+
+    if [ "$hours" -gt 0 ]; then
+        time_string="${hours} hours"
+    fi
+
+    if [ "$minutes" -gt 0 ]; then
+        if [ -n "$time_string" ]; then
+            time_string="${time_string}, "
+        fi
+        time_string="${time_string}${minutes} minutes"
+    fi
+
+    if [ "$seconds" -gt 0 ] || [ -z "$time_string" ]; then
+        if [ -n "$time_string" ]; then
+            time_string="${time_string}, and "
+        fi
+        time_string="${time_string}${seconds} seconds"
+    fi
+
+    echo -e "\nSynchronization completed in $time_string."
+}
+
 init_progress
 start_time=$(date +%s)
 
 process_directory "$drive_folder" "$local_folder"
 
-# End of the sync process
-end_time=$(date +%s)
-elapsed=$((end_time - start_time))
-
-hours=$((elapsed / 3600))
-minutes=$(( (elapsed % 3600) / 60 ))
-seconds=$((elapsed % 60))
-
-echo -e "\nSynchronization completed in $hours hours, $minutes minutes, and $seconds seconds."
+display_elapsed_time $start_time
